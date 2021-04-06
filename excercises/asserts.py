39972649bc0354c9d48f6193d0a9d489803d2bea
Oscar Giovanni <oscargiovanni@gmail.com>
2021-04-05 19:59:45 -0500

    exercises with unittest

---
+++ excercises/asserts.py
@@ -0,0 +1,10 @@
+def primera_letra(lista_de_palabras):
+    primeras_letras = []
+
+    for palabra in lista_de_palabras:
+        assert type(palabra) == str, f'{palabra} no es string'
+        assert len(palabra) > 0, 'No se permiten vacios'
+        primeras_letras.append(palabra[0])
+    return print(primeras_letras)
+
+primera_letra('uno')
--- excercises/factoriales.py
+++ excercises/factoriales.py
@@ -1,15 +1,21 @@
+import unittest
+
 def factorial(n):
     """Funcion que calcula el factorial de n.
-
     n int > 0 
     return n!
-    
     """
     print(n)
     if n == 1:
         return 1
-    
     return n * factorial(n -1 )
+
+class CajaNegratest(unittest.TestCase):
+    def test_factorial(self):
+        numero = 5
+        resultado = factorial(numero)
+        self.assertEqual(resultado,120)
+
 
 def run():
     n = int(input('Escribe un entero: '))
@@ -17,3 +23,4 @@ def run():
 
 if __name__ == "__main__":
     run()
+    unittest.main()
